35a7060dcac38c42b92b7534a332f873b5e3bd31
Leonardo Cavalcante <leocavalcante@Leonardos-MacBook-Pro.local>
2022-05-02 21:28:27 -0300

    fix apt-get

--- dependencias.sh
+++ dependencias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get git ansible -y
+sudo apt-get install git ansible -y
 git clone https://github.com/Leo-scs/pointer-app.git /tmp/pointer-app/
 
 cd /tmp/pointer-app/

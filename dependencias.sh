5e66d5a40036579fba232601f425cb04dd5ee0ec
Leonardo Cavalcante <leocavalcante@Leonardos-MacBook-Pro.local>
2022-05-02 21:09:48 -0300

    Add dependencias

--- dependencias.sh
+++ dependencias.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo yum update -y
+sudo apt-get update
+sudo apt-get git ansible -y
+git clone https://github.com/Leo-scs/pointer-app.git /tmp/pointer-app/
+
+cd /tmp/pointer-app/
+ansible pointer-playbook.yml
